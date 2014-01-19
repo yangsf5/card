5fa801dfa7913bc75b13bc83c15b4d8326cca4ff
Sheppard Y <ysf1026@gmail.com>
2014-01-19 15:48:17 +0800

    function each, replace underscore with jquery

--- public/js/hall/hall.js
+++ public/js/hall/hall.js
@@ -13,7 +13,7 @@ msgHandles['HCChat'] = function(msg) {
 msgHandles['HCRoomList'] = function(msg) {
   var rooms = $('#panel-roomlist');
   rooms.empty();
-  _.each(msg.Rooms, function(room) {
+  $.each(msg.Rooms, function(index, room) {
     rooms.append('<a href="' + room.Href + '" target="_blank">' + room.Name + '</a>' + '  <span class="badge">' + room.OnlineCount + '</span>');
     rooms.append('<br>');
   });
@@ -55,7 +55,7 @@ function addToPanel(prefix, msg) {
 function refreshPanel() {
   var panel = $('#panel-chat');
   panel.empty();
-  _.each(msgs, function(item) {
+  $.each(msgs, function(index, item) {
     panel.append('<strong>' + item.prefix + ':</strong> ');
     panel.append(item.msg).append('<br>');
   });
