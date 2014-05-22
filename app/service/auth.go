7698598a7098de4e6b3c7539b9b38e68d66f52fd
Sheppard Y <ysf1026@gmail.com>
2014-05-22 23:20:58 +0800

    init auth service

---
+++ app/service/auth.go
@@ -0,0 +1,12 @@
+// Author: sheppard(ysf1026@gmail.com) 2014-05-22
+
+package service
+
+type Auth struct {
+}
+
+func (s* Auth) ClawCallback(session int, source string, msgType int, msg interface{}) {
+}
+
+func (s* Auth) ClawStart() {
+}
